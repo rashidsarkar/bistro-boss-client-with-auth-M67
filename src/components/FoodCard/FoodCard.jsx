0b15fa70dfed180435b76e9c813c353c97db2c33
rashidsarkar <rashidrock558@gmail.com>
2023-11-13 21:50:38 +0600

    Update dependencies, add react-icons, update FoodCard component, add cart button to NavBar

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,20 +1,30 @@
-
-
-const FoodCard = ({item}) => {
-    const {name, image, price, recipe} = item;
-    return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={image} alt="Shoes" /></figure>
-            <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${price}</p>
-            <div className="card-body flex flex-col items-center">
-                <h2 className="card-title">{name}</h2>
-                <p>{recipe}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
-                </div>
-            </div>
+const FoodCard = ({ item }) => {
+  const { name, image, price, recipe } = item;
+  const handleAddtoCart = (food) => {
+    console.log(food);
+  };
+  return (
+    <div className="shadow-xl card w-96 bg-base-100">
+      <figure>
+        <img src={image} alt="Shoes" />
+      </figure>
+      <p className="absolute right-0 px-4 mt-4 mr-4 text-white bg-slate-900">
+        ${price}
+      </p>
+      <div className="flex flex-col items-center card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>{recipe}</p>
+        <div className="justify-end card-actions">
+          <button
+            onClick={() => handleAddtoCart(item)}
+            className="mt-4 border-0 border-b-4 border-orange-400 btn btn-outline bg-slate-100"
+          >
+            Add to Cart
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default FoodCard;
